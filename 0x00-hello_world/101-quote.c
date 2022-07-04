6d4bf1db13aa6789f087906240d91abdfb54cf0d
Abdul-barr Shonibare <abdulbarrshonibare@yahoo.com>
2022-07-03 18:46:00 -0700

    print to std error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,11 @@
 include <stdio.h>
-
+/**
+ * prints to std erreo
+ *
+ * Return: ret 1
+ */
 int main(void)
 {
-	putch
+write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+return(1);
 }
