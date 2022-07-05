5e41a5a0ef758b178a2d9383726fb97199878c03
Abdul-barr Shonibare <abdulbarrshonibare@yahoo.com>
2022-07-04 19:07:48 -0700

    my commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 /**
- *main - prints all possible combinations of
- *single-digit numbers 
+ *main - prints all possible combinations of single-digit numbers 
  *Return: 0 if success
- *
  */
 int main(void)
 {
