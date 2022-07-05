a9255b3c1495b7278fc4ac3d83fbdbb8e3cc3e77
Abdul-barr Shonibare <abdulbarrshonibare@yahoo.com>
2022-07-04 19:12:33 -0700

    my commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
+
 /**
- *main -prints all possible combinations of single-digit numbers 
- *Return: 0 if success
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int d;
+int n;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+if (n != 57)
+{
+putchar(',');
+putchar(' ');
 
-	for (d = 48; d < 58; d++)
-	{
-	putchar(d);
-	if (d != 57)
-	{
-	putchar(',');
-	putchar(' ');
-	}
-	}
-	putchar('\n');
-	return (0);
+}
+}
+putchar('\n');
+return (0);
+}
 }
