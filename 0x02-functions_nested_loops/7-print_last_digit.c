6760ecf6fabf34063ea57052d489b658763410a7
Abdul-barr Shonibare <abdulbarrshonibare@yahoo.com>
2022-07-05 18:57:02 -0700

    my commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,13 @@
  */
 int print_last_digit(int n)
 {
-	int l = n % 10;
-	n = n * -1;
+	int nv;
 
-	if (n <= 0 || n >= 0)
-	{
-		_putchar('0' + l);
-	}
-	return (l);
+	if (n < 0)
+		nv = -1 * (n % 10);
+	else
+		nv = n % 10;
+
+	_putchar(nv + '0');
+		return (nv);
 }
