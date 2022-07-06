def48c171b32f3843b7ae58f5a35a0ff3f738002
Abdul-barr Shonibare <abdulbarrshonibare@yahoo.com>
2022-07-05 19:28:41 -0700

    my commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,5 +10,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_times_table(int n);
+void print_times_table(int n);
 int add(int a, int b);
 void print_to_98(int n);
